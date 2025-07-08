344caa94fc59ea795417f854fd8ac621b8e0fc23
unknown <youremail@example.com>
2025-07-08 18:19:00 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-  server :{ 
-      port: 3000,
-    base : process.env.VITE_BASE_PATH ||'/itan_ramen_shop1'
-  }
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/itan_ramen_shop1',
+  server: {
+    port: 3000,
+  },
 })
