0e58247d93f3d50e996c0f3cb180f2f19853f0ca
unknown <youremail@example.com>
2026-03-08 12:01:18 +0800

    change port

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ const PORT = process.env.API_URL || 4000;
 console.log(PORT);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
